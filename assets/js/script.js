82b310497cbbe38873327655c0a46abc04592430
David Gardiner <dgardi@gmail.com>
2022-04-07 14:19:11 -0400

    updated counter and brewery list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,8 @@
 var searchBtn = $("#searchBtn")
 var brewList= $("#brewList")
 var userInput = $("#input")
-var clickableBrew = $(".newBrewery")
-var numVisits = $("<div>").length;
-
+var visitCount = $(".visitCount")
+var brewCounter = 0
 
 // Pulls in form data
 searchBtn.on("click", function(event) {
@@ -13,130 +12,24 @@ searchBtn.on("click", function(event) {
     var brewInput = userInput.val();
     console.log(brewInput);
     listCreator(brewInput);
+    countBrew(brewCounter);
 })
-
 
 // Creates a list with the data 
 var listCreator = function(brewInput) {
 var newBrew = $("<a>").addClass("newBrewery");
 newBrew.text(brewInput);
 brewList.append(newBrew);
+
    
 }
 
-// Create function to click on <a>
+// Count Up
 
-
-clickableBrew.on("click", function(e) {
-    e.preventDefault();
-    var newReview = $("<div>").addClass("newReview")
-
-    
-
-})
-
-
-
-
-// 
-
-
-
-
-
-// // Define Variables
-
-// const search = document.getElementById("search")
-// const matchList = document.getElementById("match-list")
-// var searchBtn = document.getElementById("searchBtn")
-// var searchForm = document.getElementById('search-form')
-
-
-
-// // // Pulls data for Atlanta breweries
-// // var getBreweries = function () {
-// //     var APIUrl = "https://api.openbrewerydb.org/breweries?by_city=" + # + "&per_page=50"
-
-// //     fetch(APIUrl).then(function (response) {
-// //         if (response.ok) {
-// //             return response.json().then(function (data) {
-// //                 console.log(data);
-// //                 #PLACEHOLDER(data);
-// //             })
-// //         } else {
-// //             alert("Error: " + response.status);
-// //         }
-// //     })
-// // };
-
-
-// // // getBreweries("atlanta");
-
-// // citySearch.on("submit", getCityName)
-
-
-// // Get Breweries Search Info
-
-// function getSearch(e){
-//     if (!search.value || search.value === '') {
-//         alert("Choose a city please!");
-//         return
-//     }
-//     e.preventDefault()
-//     var searchText = search.value.trim();
-//     console.log(searchText);
-//     searchBrewery(searchText)
-//     search.value = ''
-// }
-
-// console.log(search.name);
-
-
-// const searchBrewery = async searchText => {
-//     const res = await fetch(`https://api.openbrewerydb.org/breweries/search?query=${searchText}`);
-//     const breweries = await res.json();
-
-//     console.log(breweries)
-
-//     // Get matches to current text input (so it should change every time we type something)
-
-//     let matches = breweries.filter(breweries => {
-//         const regex = new RegExp(`^${searchText}`, 'gi');
-//         return breweries.name.match(regex)
-//     }); // Loop through an array and returns array based on condition (or multiple)
-
-//     if(searchText.length === 0){
-//         matches = [];
-//         matchList.innerHTML = "";
-//     }
-
-//     console.log();
-// }
-
-// // Show Results in HTML
-// const outputHtml = matches => {
-//     if(matches.length > 0) {
-//         const html = matches.map(match => `
-//         <div class="card card-body mb-1">
-//             <h4>${match.name}</h4>
-//         </div>
-//         `)
-//         .join('');
-
-//     matchList.innerHTML = html;
-
-//     }
-// }
-
-
-
-
-    
-
-
-
-
-// search.addEventListener("input", () => searchBrewery(search.value));
+var countBrew = function() {
+    brewCounter++;
+    visitCount.text(brewCounter);
+}
 
 
 
